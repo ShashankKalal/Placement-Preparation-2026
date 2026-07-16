95be16557a5729d1af28754804212732c94c1bca
ShashankKalal <shashankkalal07@gmail.com>
2026-07-17 00:22:47 +0530

    Phoenix Day 3

---
+++ Java/PosNegZer.java
@@ -0,0 +1,14 @@
+public class PosNegZer {
+    public static void main(String[] args) {
+        int number = 10; // You can change this number to test with different values
+
+        if (number > 0) {
+            System.out.println(number + " is positive.");
+        } else if (number < 0) {
+            System.out.println(number + " is negative.");
+        } else {
+            System.out.println(number + " is zero.");
+        }
+    }
+    
+}
